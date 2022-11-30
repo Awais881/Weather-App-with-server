198e34b636207413bbba1a553d5e69735fa108d1
zaidasim2197 <https://github.com/zaidasim2197/Weather-Api-React.git>
2022-11-30 22:26:40 +0500

    new build added

--- web/src/weather.jsx
+++ web/src/weather.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from "axios"
 import './weather.css'
-import { useState,useEffect } from 'react'
+import { useState } from 'react'
 
 let baseUrl = ``;
 if (window.location.href.split(":")[0] === "http") {
@@ -18,7 +18,7 @@ const Weather = () => {
     const submitHandler = (e) => {
         e.preventDefault();
 
-        axios.get(`${baseUrl}/weather`)
+        axios.get(`${baseUrl}/weather/${cityName}`)
         .then(response => {
           console.log("response: ", response.data);
           console.log("I am click handler")
@@ -55,20 +55,28 @@ const Weather = () => {
         </div> 
       
         {(weatherData === null) ? null :
-
+          <div>
+             <div className='center'>
+            <div className="temp">{Math.round(weatherData?.temp_c)}°C</div>
+        </div>
         
      
-    <div id="forcastDiv" className="forcastDiv"> 
+       <div id="forcastDiv" className="forcastDiv"> 
         <div className="forcastCard">
-        <div className="city">{weatherData.city}</div>
-            <div className="temp_c">{Math.round(weatherData?.temp_c)}°C</div>
-        <div id="time">{new Date().toDateString()}</div>
-        <div className="day">Humidity: {weatherData.humidity}%</div>
-            <div className="min">Min : {weatherData.max_temp_c}°C</div>
+        <div className="city"><h3>{weatherData.city}</h3></div> <br />
+      <div className='flex'>
+        <div className='left'>
+          <div className="time">{new Date().toDateString()}</div>
+
+          <div className="day">Humidity: {weatherData.humidity}%</div>
+          </div>
+          <div className='right'>
+            <div className="min">Min : {weatherData.max_temp_c}°C</div> 
             <div className="max">Max: {weatherData.min_temp_c}°C</div>
+            </div>
+            </div> 
         </div> 
-
-
+        </div> 
 
     </div> 
 }
